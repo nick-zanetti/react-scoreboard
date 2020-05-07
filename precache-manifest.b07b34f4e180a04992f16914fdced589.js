fc34fa4b8c4463037f12817a71959194f970a164
Nick Zanetti <nzanetti44@gmail.com>
2020-05-06 21:35:08 -0400

    Updates

---
+++ precache-manifest.b07b34f4e180a04992f16914fdced589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9955495a48ef946e0116d45e0fefb26",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "91112dc70e9ee4caec5a",
+    "url": "/react-scoreboard/static/css/main.43c19067.chunk.css"
+  },
+  {
+    "revision": "bbbd96484ef60204a048",
+    "url": "/react-scoreboard/static/js/2.7bc39ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-scoreboard/static/js/2.7bc39ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91112dc70e9ee4caec5a",
+    "url": "/react-scoreboard/static/js/main.303025d3.chunk.js"
+  },
+  {
+    "revision": "3c7c7b84407944ddcf31",
+    "url": "/react-scoreboard/static/js/runtime-main.f18d59d8.js"
+  }
+]);
